704b777af4645748586a8dccf24d95d68d1a5b47
ZippCodder <deonisawesome777>
2021-05-06 16:35:55 +0000

    ...

--- index.js
+++ index.js
@@ -3,8 +3,24 @@ const fs = require("fs");
 const crypto = require("crypto");
 const net = require("net");
 
+// Keep record of clients...
+
+const CURRENT_CLIENTS = [];
+
+// Establish and manage websocket connection with clients...
+
 const server = net.createServer((connection) => {
+
+connection.on("error",() => {
+ console.log("\x1b[31m%s\x1b","TRANSMISSION ERROR OCURRED! CLIENT_SOCKET: " + connection.remoteAddress); 
+})
+
+connection.on("close",() => {
+console.log("\x1b[33m%s\x1b[0m","Client " + connection.remoteAddress + "has disconnected!");
+});
+
  connection.on("data",(data) => {
+     if (!CURRENT_CLIENTS.includes(connection.remoteAddress)) {
   let request = data.toString();
   let lines = request.split("\r\n");
   let startLine = lines.shift().split(" ");
@@ -19,8 +35,6 @@ const server = net.createServer((connection) => {
     let hdr = i.split(": ");
     headers[hdr[0]] = hdr[1];
   }
-
-  console.log(request);
 
   let _OK_HEADER_ = "HTTP/1.1 200 OK\r\n";
   let _ISE_HEADER_ = "HTTP/1.1 500 Internal Server Error\r\n";
@@ -38,13 +52,33 @@ const server = net.createServer((connection) => {
       }
      }); 
    } else if (path == "/chat") {
-      let response = _PROTO_SWITCH_HEADER_.concat("Upgrade: websocket\r\n","Connection: Upgrade\r\n","Sec-WebSocket-Accept: " + crypto.createHash("sha1").update(headers["Sec-WebSocket-Key"] + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").digest("base64"),"\r\n");
+         CURRENT_CLIENTS.push(connection.remoteAddress);
+  console.log("\x1b[33m%s\x1b[0m",`A new client connected: ${connection.remoteAddress}`);
 
-    connection.end(response);
-    console.log(response + "\r\n");
+          let response = _PROTO_SWITCH_HEADER_.concat("Upgrade: websocket\r\n","Connection: Upgrade\r\n","Sec-WebSocket-Accept: " + crypto.createHash("sha1").update(headers["Sec-WebSocket-Key"] + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").digest("base64"),"\r\n\r\n");
+    connection.write(response);
+    console.log("\x1b[34m%s\x1b[0m","Handshake successful!"); 
    }
+} else { 
+ 
+ // @TODO: Decipher and route message
+
+}
  });
 });
+
+
+// Parse message...
+
+function parseMessage(msg,client) {
+
+}
+
+// Process message...
+
+function processMessage(msg,client) {
+	
+}
 
 server.listen(3000,() => {
  console.log("\x1b[32m%s\x1b[0m","Server is listening...");
