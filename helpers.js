699fb2418977a140c66202e431b8abb4a108758b
ZippCodder <deonisawesome777>
2021-05-31 19:00:49 +0000

    main commit

---
+++ helpers.js
@@ -0,0 +1,8 @@
+let lib = {};
+
+lib.bite = function (b,l=8) {
+if (b.length => 8) return b;
+return "0".repeat(l-b.length) + b;
+}
+
+module.exports = lib;
--- index.js
+++ index.js
@@ -2,17 +2,20 @@ const http = require("http");
 const fs = require("fs");
 const crypto = require("crypto");
 const net = require("net");
+const { v4: uuid } = require("uuid");
+const { bite } = require("./helpers.js");
+const { gzip, gunzip } = require("zlib");
 
 // Keep record of clients...
 
-const CURRENT_CLIENTS = [];
+const CONNECTED_CLIENTS = {};
 
 // Establish and manage websocket connection with clients...
 
 const server = net.createServer((connection) => {
 
  connection.on("data",(data) => {
-     if (!CURRENT_CLIENTS.includes(connection.remoteAddress)) {
+     if (!connection._id) {
   let request = data.toString();
   let lines = request.split("\r\n");
   let startLine = lines.shift().split(" ");
@@ -20,6 +23,13 @@ const server = net.createServer((connection) => {
   let method = startLine[0];
   let path = startLine[1];
   let headers = {};
+  let query = {};
+  if (/\?/.test(path)) {
+   startLine[1].split("?").split("&").forEach(v => {
+     let params = v.split("=");
+     query[params[0]] = params[1];
+   });
+}
 
  const hdrs = lines.filter((h) => { return h.length > 1 });
   for (let i of hdrs) {
@@ -31,7 +41,7 @@ const server = net.createServer((connection) => {
   let _ISE_HEADER_ = "HTTP/1.1 500 Internal Server Error\r\n";
   let _PROTO_SWITCH_HEADER_ = "HTTP/1.1 101 Switching Protocols\r\n";
   
-   if (path == "/") {
+   if (/^\//.test(path)) {
      fs.readFile("./index.html","ascii",(err,data) => {
        if (!err && data) {
           let page = _OK_HEADER_.concat("Content-Type: text/html; charset=utf-8","\r\n\r\n",data.replace("\n",""));
@@ -42,27 +52,32 @@ const server = net.createServer((connection) => {
         connection.end();
       }
      }); 
-   } else if (path == "/chat") {
-         CURRENT_CLIENTS.push(connection.remoteAddress);
-  console.log("\x1b[33m%s\x1b[0m",`A new client connected: ${connection.remoteAddress}`);
+   } else if (/^\/chat/.test(path)) {
+         connection._id = uuid();
+      CONNECTED_CLIENTS[connection._id] = connection;
+  console.log("\x1b[33m%s\x1b[0m",`A new client connected: ${connection._id}`);
 
-          let response = _PROTO_SWITCH_HEADER_.concat("Upgrade: websocket\r\n","Connection: Upgrade\r\n","Sec-WebSocket-Accept: " + crypto.createHash("sha1").update(headers["Sec-WebSocket-Key"] + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").digest("base64"),"\r\n\r\n");
-    connection.write(response);
+          let response = _PROTO_SWITCH_HEADER_.concat("Upgrade: websocket\r\n","Connection: Upgrade\r\n","Sec-WebSocket-Accept: " + crypto.createHash("sha1").update(headers["Sec-WebSocket-Key"] + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").digest("base64"),"\r\n");
+
+   if (headers["Sec-WebSocket-Extensions"]) {
+      response = response.concat("Sec-WebSocket-Extensions: permessage-deflate\r\n").concat("Accept-Encoding: gzip\r\n");
+   }
+
+    connection.write(response.concat("\r\n\r\n"));
     console.log("\x1b[34m%s\x1b[0m","Handshake successful!"); 
    }
-} else { 
- 
+} else {
  // @TODO: Decipher and route message
-
+parseMessage(data,connection._id);
 }
  });
 
-connection.on("error",() => {
- console.log("\x1b[31m%s\x1b","TRANSMISSION ERROR OCURRED! CLIENT_SOCKET: " + connection.remoteAddress); 
+connection.on("error",(err) => {
+ console.log("\x1b[31m%s\x1b","TRANSMISSION ERROR OCURRED! CLIENT_SOCKET: " + connection._id + " " + err); 
 })
 
 connection.on("close",() => {
-console.log("\x1b[33m%s\x1b[0m","Client " + connection.remoteAddress + "has disconnected!");
+console.log("\x1b[33m%s\x1b[0m","Client " + connection._id + "has disconnected!");
 });
 
 });
@@ -70,14 +85,74 @@ console.log("\x1b[33m%s\x1b[0m","Client " + connection.remoteAddress + "has disc
 
 // Parse message...
 
-function parseMessage(msg,client) {
+function parseMessage(message,clientId) {
+let data = new Uint8Array(message);
 
+let frame = {
+_first_bite: data[0].toString(2),
+_second_bite: data[2].toString(2);
+FRAME: data;
+FIN: Number(this._first_bite[0]),
+OPCODE: parseInt(this._first_bite.slice(4),2),
+COMPRESSED: this._first_bite[1],
+MASK: this._second_bite[0],
+RANGES: [...data.slice(2,4),...data.slice(2,10)]
+};
+
+let len = parseInt(frame.second_bite.slice(1),2);
+frame.LENGTHS = [len,parseInt(data[2].toString(2) + data[3].toString(2),2),parseInt(data.slice(2,10).reduce((a,v) => { return a+v.toString(2) },""))];
+if (len <= 125) {
+frame.PAYLOAD_LENGTH = len;
+frame.MASK_KEY = data.slice(2,6);
+frame.PAYLOAD = data.slice(6,frame.PAYLOAD_LENGTH-1);
+} else if (len < 127) {
+frame.PAYLOAD_LENGTH = frame.LENGTHS[1];
+frame.MASK_KEY = data.slice(4,8);
+frame.PAYLOAD = data.slice(8,frame.PAYLOAD_LENGTH-1);
+} else {
+frame.PAYLOAD_LENGTH = frame.LENGTHS[2];
+frame.MASK_KEY = data.slice(10,14);
+frame.PAYLOAD = data.slice(14,frame.PAYLOAD_LENGTH-1);
+}
+
+if (frame.COMPRESSED) {
+gunzip(frame.PAYLOAD,(err,buffer) => {
+ frame.PAYLOAD = new Uint8Array(buffer);
+});
+}
+
+frame.UNMASKED_PAYLOAD = new Uint8Array(frame.PAYLOAD_LENGTH);
+for (let i = 0; i < frame.PAYLOAD_LENGTH; i++) {
+frame.UNMASKED_PAYLOAD[i] = frame.PAYLOAD[i] ^ frame.MASK_KEY[i % 4];
+}
+processMessage(frame,clientId);
 }
 
 // Process message...
 
-function processMessage(msg,client) {
-	
+function processMessage(frame,clientId) {
+ let resFrame = {};
+ resFrame._fin = frame.FIN;
+ resFrame._exts = (frame.COMPRESSED) ? "100":"000";
+ resFrame._opcode = bite(frame.OPCODE.toString(2),4);
+ resFrame._payload_length = bite(frame.FRAME[1].toString(2).slice(1));
+ resFrame._payload = frame.UNMASKED_PAYLOAD;
+ 
+ if (resFrame._payload_length <= 125) {
+resFrame._ext_length = [resFrame._payload_length];
+} else if (resFrame._payload_length < 127) {
+resFrame._ext_length = [resFrame._payload_length,...frame.FRAME.slice(2,4)];
+} else {
+resFrame._ext_length = [resFrame._payload_length,...frame.FRAME.slice(2,10)];
+}
+
+let response = new Uint8Array([parseInt(resFrame._fin+resFrame._exts+resFrame._opcode,2),...resFrame._ext_length,...resFrame._payload]);
+
+console.log(frame);
+
+for (let i in CONNECTED__CLIENTS) {
+if (i !== clientId) CONNECTED_CLIENTS[i].write(response);
+}
 }
 
 server.listen(3000,() => {
